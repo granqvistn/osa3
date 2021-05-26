5609fa9a0efee6e14fdf1c14f3d567b381ef23a0
Niklas <niklas.granqvist@hotmail.com>
2021-05-26 19:46:23 +0300

    valmis osa 3

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 require('dotenv').config()
 
 var morgan = require('morgan')
-morgan.token('body', (req, res) => JSON.stringify(req.body));
+morgan.token('body', (req) => JSON.stringify(req.body))
 
 const app = express()
 const Person = require('./models/person')
@@ -17,60 +17,57 @@ app.use(morgan(':method :url :status :res[content-length] :response-time ms  :bo
 app.use(cors())
 app.use(express.static('build'))
 
-const genID = () => {
-    return Math.floor(Math.random() * 1000)
 
-}
-
+/*
 let persons = [
-    { 
-        "name": "Arto Hellas", 
+    {
+        "name": "Arto Hellas",
         "number": "040-123456",
         "id": 1
       },
-      { 
-        "name": "Ada Lovelace", 
+      {
+        "name": "Ada Lovelace",
         "number": "39-44-5323523",
         "id": 2
       },
-      { 
-        "name": "Dan Abramov", 
+      {
+        "name": "Dan Abramov",
         "number": "12-43-234345",
         "id": 3
       },
-      { 
-        "name": "Mary Poppendieck", 
+      {
+        "name": "Mary Poppendieck",
         "number": "39-23-6423122",
         "id": 4
       }
-]
+]*/
 
 app.get('/api/persons', (req, response, next) => {
-    Person.find({})
-        .then(persons => {
-            response.json(persons)
-        })
-        .catch(error => next(error))
+  Person.find({})
+    .then(persons => {
+      response.json(persons)
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (req, response) => {
   Person.findById(req.params.id)
-  .then(note => {
-    if (note) {
-      response.json(note)
-    } else {
-      response.status(404).end()
-    }
-  })
-  .catch(error => {
-    console.log(error)
-    response.status(400).send({ error: 'malformatted id' })
-  })
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
+    })
 })
 
 app.get('/info', (req, response) => {
-    const date = new Date
-    Person.find({})
+  const date = new Date
+  Person.find({})
     .then(persons => {
       response.end(`Phonebook has info for ${persons.length} people \n${date}`)
     })
@@ -78,26 +75,24 @@ app.get('/info', (req, response) => {
 })
 
 app.post('/api/persons', (req, response, next) => {
-    
-    const body = req.body
-    console.log(body)
-    
 
-    const person = new Person({
-        name : body.name,
-        number : body.number        
+  const body = req.body
+  console.log(body)
+
+  const person = new Person({
+    name : body.name,
+    number : body.number
+  })
+  person
+    .save()
+    .then(savedPerson => {
+      response.json(savedPerson.toJSON())
     })
-    person
-      .save()
-        .then(savedPerson => {
-          response.json(savedPerson.toJSON())
-        })
-        .catch(error => next(error))
-    
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, response, next) => {
-    Person.findByIdAndRemove(req.params.id)
+  Person.findByIdAndRemove(req.params.id)
     .then(result => {
       response.status(204).end()
     })
@@ -106,38 +101,38 @@ app.delete('/api/persons/:id', (req, response, next) => {
 
 
 app.put('/api/persons/:id', (req, response, next) => {
-    const body = req.body
-  
-    const person = {
-      name: body.name,
-      number: body.number,
-    }
-  
-    Person.findByIdAndUpdate(req.params.id, person, { new: true })
-      .then(updatedPerson => {
-        response.json(updatedPerson)
-      })
-      .catch(error => next(error))
-  })
+  const body = req.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
 
 
 
 const errorHandler = (error, req, response, next) => {
-    console.error(error.message)
-  
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-      console.log(error.message)
-      return response.status(400).json({error : error.message})
-    }
-  
-    next(error)
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    console.log(error.message)
+    return response.status(400).json({error : error.message})
   }
-  app.use(errorHandler)
+
+  next(error)
+}
+app.use(errorHandler)
 
 
-  const PORT = process.env.PORT || 3001
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
--- mongo.js
+++ mongo.js
@@ -20,24 +20,24 @@ const personSchema = new mongoose.Schema({
 const Person = mongoose.model('Person', personSchema)
 console.log(process.argv.length)
 if (process.argv.length > 3){
-    const person = new Person({
-        name: process.argv[3],
-        number: process.argv[4]
-    })
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4]
+  })
 
-    person.save().then(res => {
-        console.log(`added ${person.name} number ${person.number} to phonebook`)
-        mongoose.connection.close()
-    })
+  person.save().then(res => {
+    console.log(`added ${person.name} number ${person.number} to phonebook`)
+    mongoose.connection.close()
+  })
 }
 
 if (process.argv.length === 3){
-    Person.find({}).then(res => {
-        console.log("phonebook")
-        res.forEach(per => {
-            console.log(per.name, per.number)
-        })
-        mongoose.connection.close()
+  Person.find({}).then(res => {
+    console.log('phonebook')
+    res.forEach(per => {
+      console.log(per.name, per.number)
     })
+    mongoose.connection.close()
+  })
 }
 
